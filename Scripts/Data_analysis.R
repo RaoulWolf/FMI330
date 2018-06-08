75380cbf6ef11b848fa1658bdb0a0c6daa37575a
Raoul Wolf <Raoul.Wolf@niva.no>
2018-06-08 09:32:00 +0200

    Updated for the use of the original data set

--- Scripts/Data_analysis.R
+++ Scripts/Data_analysis.R
@@ -4,21 +4,31 @@ library(drc)
 library(tidyverse)
 library(readxl)
 
-data_raw <- read_excel("Data/Data-FMI310_tidy.xlsx", 
-                       sheet = "One_stressor_tidy")
+data_raw <- read_excel("Data/Data-FMI310.xlsx", 
+                       sheet = "One stressor -SM")
 
 data_raw
 
 data <- data_raw %>% 
+  rename(Stressor_A = `Stressor A`, 
+         Fronds_number = `Fronds Number`, 
+         Growth_rate = `Growth rate (pr day)`,
+         Growth_inhibition = `Growth inhibition (%)`, 
+         Fv_Fm = `Fv/Fm`, 
+         PS_II_inhibition = `PS II inhibition (%)`, 
+         ROS_formation = `ROS formation`, 
+         ROS_formation_fold_increase = `ROS formation (fold increase)`) %>% 
   mutate(Replicate = as.factor(Replicate)) %>% 
   arrange(Stressor_A, Replicate)
 
 data
 
+rm(data_raw)
+
 data %>% 
   ggplot(mapping = aes(x = Stressor_A, y = Fronds_number)) +
   geom_point(alpha = 0.5) +
-  labs(x = "Concentration", 
+  labs(x = "Stressor A", 
        y = "Fronds number") + 
   theme_bw()
 
@@ -37,14 +47,24 @@ pred <- tibble(Stressor_A = seq(from = min(data$Stressor_A),
   as_tibble()
 
 data %>% 
+  group_by(Stressor_A) %>% 
+  summarize(Fronds_number_mean = mean(Fronds_number), 
+            Fronds_number_SE = sd(Fronds_number) / sqrt(n())) %>% 
   ggplot() +
-  geom_point(mapping = aes(x = Stressor_A, y = Fronds_number), alpha = 0.5) +
-  geom_vline(xintercept = data_drm$coefficients[4], color = "red") +
+  geom_point(mapping = aes(x = Stressor_A, y = Fronds_number_mean), 
+             color = "dimgrey") +
+  geom_errorbar(mapping = aes(x = Stressor_A, 
+                              ymin = Fronds_number_mean - Fronds_number_SE,
+                              ymax = Fronds_number_mean + Fronds_number_SE),
+                color = "dimgrey", width = 0) + 
+  geom_vline(xintercept = data_drm$coefficients[4], 
+             color = "red", linetype = 3) +
   geom_hline(yintercept = ((data_drm$coefficients[3] - data_drm$coefficients[2]) / 2) + data_drm$coefficients[2], 
-             color = "red") +
+             color = "red", linetype = 3) +
   geom_ribbon(mapping = aes(x = Stressor_A, ymin = lwr, ymax = upr), 
               data = pred, alpha = 0.2) +
-  geom_line(mapping = aes(x = Stressor_A, y = pred), data = pred, size = 1) +
-  labs(x = "Concentration", 
+  geom_line(mapping = aes(x = Stressor_A, y = pred), 
+            data = pred, size = 1) +
+  labs(x = "Stressor A", 
        y = "Fronds number") + 
   theme_bw()
